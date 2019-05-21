7f15fcbf0df74d68c5259a4c4b4957b99489b6bd
horsicq <horsicq@gmail.com>
2019-05-21 18:27:23 +0200

    Fix

--- ELF/elfwidget.cpp
+++ ELF/elfwidget.cpp
@@ -622,6 +622,14 @@ bool ELFWidget::createSectionTable(int type, QTableWidget *pTableWidget, const F
             slHeader.append(tr(""));
             pTableWidget->setColumnCount(nRecordCount+1);
             pTableWidget->setColumnWidth(0,nSymbolWidth*4);
+            pTableWidget->setColumnWidth(1,nSymbolWidth*8);
+            pTableWidget->setColumnWidth(2,nSymbolWidth*8);
+            pTableWidget->setColumnWidth(3,nSymbolWidth*12);
+            pTableWidget->setColumnWidth(4,nSymbolWidth*12);
+            pTableWidget->setColumnWidth(5,nSymbolWidth*12);
+            pTableWidget->setColumnWidth(6,nSymbolWidth*12);
+            pTableWidget->setColumnWidth(7,nSymbolWidth*12);
+            pTableWidget->setColumnWidth(8,nSymbolWidth*12);
             break;
 
         default:
