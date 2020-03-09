a717bae642e03af87bfc0cda9f321e164b1894a8
horsicq <horsicq@gmail.com>
2020-03-09 11:07:19 +0100

    Fix

---
+++ PE/importheaderwidget.cpp
@@ -0,0 +1,208 @@
+// copyright (c) 2017-2020 hors<horsicq@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "importheaderwidget.h"
+#include "ui_importheaderwidget.h"
+
+ImportHeaderWidget::ImportHeaderWidget(QWidget *parent):
+    FormatWidget(parent),
+    ui(new Ui::ImportHeaderWidget)
+{
+    ui->setupUi(this);
+}
+
+ImportHeaderWidget::ImportHeaderWidget(QIODevice *pDevice, FW_DEF::OPTIONS *pOptions, quint32 nNumber, QWidget *parent):
+    FormatWidget(pDevice,pOptions,nNumber,parent),
+    ui(new Ui::ImportHeaderWidget)
+{
+    ui->setupUi(this);
+
+    setData(pDevice,pOptions,nNumber);
+}
+
+ImportHeaderWidget::~ImportHeaderWidget()
+{
+    delete ui;
+}
+
+void ImportHeaderWidget::clear()
+{
+    bInit=false;
+
+    memset(lineEdit_IMPORT_HEADER,0,sizeof lineEdit_IMPORT_HEADER);
+    memset(invWidget,0,sizeof invWidget);
+
+    pSubDevice=nullptr;
+
+    ui->checkBoxReadonly->setChecked(true);
+}
+
+void ImportHeaderWidget::setData(QIODevice *pDevice, FW_DEF::OPTIONS *pOptions, quint32 nNumber)
+{
+    FormatWidget::setData(pDevice,pOptions,nNumber);
+}
+
+void ImportHeaderWidget::reload()
+{
+    clear();
+
+    ui->checkBoxReadonly->setEnabled(!isReadonly());
+
+    reloadData();
+}
+
+bool ImportHeaderWidget::_setValue(QVariant vValue, int nStype, int nNdata, int nVtype,int nPosition)
+{
+    Q_UNUSED(nVtype)
+
+    bool bResult=false;
+
+    quint64 nValue=vValue.toULongLong();
+    QString sValue=vValue.toString();
+
+    if(getDevice()->isWritable())
+    {
+        XPE pe(getDevice(),getOptions()->bIsImage,getOptions()->nImageBase);
+
+        if(pe.isValid())
+        {
+            switch(nStype)
+            {
+                case SPE::TYPE_IMAGE_SECTION_HEADER:
+                    switch(nNdata)
+                    {
+                        case N_IMAGE_IMPORT::OriginalFirstThunk:            invWidget[INV_OriginalFirstThunk]->setAddressAndSize(&pe,pe.getBaseAddress()+(quint32)nValue,0);    break;
+                        case N_IMAGE_IMPORT::Name:                          invWidget[INV_Name]->setAddressAndSize(&pe,pe.getBaseAddress()+(quint32)nValue,0);                  break;
+                        case N_IMAGE_IMPORT::FirstThunk:                    invWidget[INV_FirstThunk]->setAddressAndSize(&pe,pe.getBaseAddress()+(quint32)nValue,0);            break;
+                    }
+
+                    break;
+            }
+
+            switch(nStype)
+            {
+                case SPE::TYPE_IMAGE_SECTION_HEADER:
+                    switch(nNdata)
+                    {
+                        case N_IMAGE_SECTION_HEADER::Name:                  pe.setSection_NameAsString((quint32)nPosition,sValue);                  break;
+                        case N_IMAGE_SECTION_HEADER::VirtualSize:           pe.setSection_VirtualSize((quint32)nPosition,(quint32)nValue);          break;
+                        case N_IMAGE_SECTION_HEADER::VirtualAddress:        pe.setSection_VirtualAddress((quint32)nPosition,(quint32)nValue);       break;
+                        case N_IMAGE_SECTION_HEADER::SizeOfRawData:         pe.setSection_SizeOfRawData((quint32)nPosition,(quint32)nValue);        break;
+                        case N_IMAGE_SECTION_HEADER::PointerToRawData:      pe.setSection_PointerToRawData((quint32)nPosition,(quint32)nValue);     break;
+                        case N_IMAGE_SECTION_HEADER::PointerToRelocations:  pe.setSection_PointerToRelocations((quint32)nPosition,(quint32)nValue); break;
+                        case N_IMAGE_SECTION_HEADER::PointerToLinenumbers:  pe.setSection_PointerToLinenumbers((quint32)nPosition,(quint32)nValue); break;
+                        case N_IMAGE_SECTION_HEADER::NumberOfRelocations:   pe.setSection_NumberOfRelocations((quint32)nPosition,(quint16)nValue);  break;
+                        case N_IMAGE_SECTION_HEADER::NumberOfLinenumbers:   pe.setSection_NumberOfLinenumbers((quint32)nPosition,(quint16)nValue);  break;
+                        case N_IMAGE_SECTION_HEADER::Characteristics:       pe.setSection_Characteristics((quint32)nPosition,(quint32)nValue);      break;
+                    }
+
+                    ui->widgetHex_IMPORT_HEADER->reload();
+
+                    break;
+            }
+
+            bResult=true;
+        }
+    }
+
+    return bResult;
+}
+void ImportHeaderWidget::setReadonly(bool bState)
+{
+    setLineEditsReadOnly(lineEdit_IMPORT_HEADER,N_IMAGE_SECTION_HEADER::__data_size,bState);
+}
+
+void ImportHeaderWidget::blockSignals(bool bState)
+{
+    _blockSignals((QObject **)lineEdit_IMPORT_HEADER,N_IMAGE_SECTION_HEADER::__data_size,bState);
+}
+
+void ImportHeaderWidget::adjustHeaderTable(int type, QTableWidget *pTableWidget)
+{
+    int nSymbolWidth=getSymbolWidth();
+
+    pTableWidget->setColumnWidth(HEADER_COLUMN_OFFSET,nSymbolWidth*4);
+    pTableWidget->setColumnWidth(HEADER_COLUMN_TYPE,nSymbolWidth*6);
+
+    switch(type)
+    {
+        case SPE::TYPE_IMAGE_SECTION_HEADER:
+            pTableWidget->setColumnWidth(HEADER_COLUMN_NAME,nSymbolWidth*16);
+            pTableWidget->setColumnWidth(HEADER_COLUMN_VALUE,nSymbolWidth*8);
+            pTableWidget->setColumnWidth(HEADER_COLUMN_INFO,nSymbolWidth*16);
+            break;
+    }
+}
+
+void ImportHeaderWidget::on_checkBoxReadonly_toggled(bool checked)
+{
+    setReadonly(checked);
+}
+
+void ImportHeaderWidget::reloadData()
+{
+    XPE pe(getDevice(),getOptions()->bIsImage,getOptions()->nImageBase);
+
+    if(pe.isValid())
+    {
+        if(!bInit)
+        {
+            bInit=createHeaderTable(SPE::TYPE_IMPORT,ui->tableWidget_IMPORT_HEADER,N_IMAGE_IMPORT::records,lineEdit_IMPORT_HEADER,N_IMAGE_IMPORT::__data_size,getNumber());
+
+            invWidget[INV_OriginalFirstThunk]=createInvWidget(ui->tableWidget_IMPORT_HEADER,SPE::TYPE_IMPORT,N_IMAGE_IMPORT::OriginalFirstThunk,InvWidget::TYPE_HEX);
+            invWidget[INV_Name]=createInvWidget(ui->tableWidget_IMPORT_HEADER,SPE::TYPE_IMPORT,N_IMAGE_IMPORT::Name,InvWidget::TYPE_HEX);
+            invWidget[INV_FirstThunk]=createInvWidget(ui->tableWidget_IMPORT_HEADER,SPE::TYPE_IMPORT,N_IMAGE_IMPORT::FirstThunk,InvWidget::TYPE_HEX);
+        }
+
+        blockSignals(true);
+
+        XPE_DEF::IMAGE_IMPORT_DESCRIPTOR idh=pe.getImportDescriptor(getNumber());
+
+        lineEdit_IMPORT_HEADER[N_IMAGE_IMPORT::OriginalFirstThunk]->setValue(idh.OriginalFirstThunk);
+        lineEdit_IMPORT_HEADER[N_IMAGE_IMPORT::TimeDateStamp]->setValue(idh.TimeDateStamp);
+        lineEdit_IMPORT_HEADER[N_IMAGE_IMPORT::ForwarderChain]->setValue(idh.ForwarderChain);
+        lineEdit_IMPORT_HEADER[N_IMAGE_IMPORT::Name]->setValue(idh.Name);
+        lineEdit_IMPORT_HEADER[N_IMAGE_IMPORT::FirstThunk]->setValue(idh.FirstThunk);
+
+        invWidget[INV_OriginalFirstThunk]->setAddressAndSize(&pe,pe.getBaseAddress()+idh.OriginalFirstThunk,0);
+        invWidget[INV_Name]->setAddressAndSize(&pe,pe.getBaseAddress()+idh.Name,0);
+        invWidget[INV_FirstThunk]->setAddressAndSize(&pe,pe.getBaseAddress()+idh.FirstThunk,0);
+
+        qint64 nOffset=pe.getSectionHeaderOffset(getNumber());
+        qint64 nSize=pe.getSectionHeaderSize();
+        qint64 nAddress=pe.offsetToRelAddress(nOffset);
+
+        loadHexSubdevice(nOffset,nSize,nAddress,&pSubDevice,ui->widgetHex_IMPORT_HEADER);
+
+        blockSignals(false);
+
+        setReadonly(ui->checkBoxReadonly->isChecked());
+    }
+}
+
+void ImportHeaderWidget::on_tableWidget_IMPORT_HEADER_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn)
+{
+    Q_UNUSED(currentRow)
+    Q_UNUSED(currentColumn)
+    Q_UNUSED(previousRow)
+    Q_UNUSED(previousColumn)
+
+    setHeaderTableSelection(ui->widgetHex_IMPORT_HEADER,ui->tableWidget_IMPORT_HEADER);
+}
