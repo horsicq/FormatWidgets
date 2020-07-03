b51aff7659b04dce47bdd5a005b8912b21f8facd
horsicq <horsicq@gmail.com>
2020-07-03 15:04:41 +0200

    Fix

---
+++ LE/lesectionheaderwidget.cpp
@@ -0,0 +1,88 @@
+// copyright (c) 2019-2020 hors<horsicq@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "machsectionheaderwidget.h"
+#include "ui_sectionheaderwidget.h"
+
+
+ELFSectionHeaderWidget::ELFSectionHeaderWidget(QWidget *parent)
+{
+
+}
+
+ELFSectionHeaderWidget::ELFSectionHeaderWidget(QIODevice *pDevice, FW_DEF::OPTIONS *pOptions, quint32 nNumber, qint64 nOffset,qint32 nType, QWidget *parent)
+{
+
+}
+
+ELFSectionHeaderWidget::~ELFSectionHeaderWidget()
+{
+
+}
+
+void ELFSectionHeaderWidget::clear()
+{
+
+}
+
+void ELFSectionHeaderWidget::setData(QIODevice *pDevice, FW_DEF::OPTIONS *pOptions, quint32 nNumber, qint64 nOffset)
+{
+
+}
+
+void ELFSectionHeaderWidget::reload()
+{
+
+}
+
+bool ELFSectionHeaderWidget::_setValue(QVariant vValue, int nStype, int nNdata, int nVtype, int nPosition, qint64 nOffset)
+{
+    return false;
+}
+
+void ELFSectionHeaderWidget::setReadonly(bool bState)
+{
+
+}
+
+void ELFSectionHeaderWidget::blockSignals(bool bState)
+{
+
+}
+
+void ELFSectionHeaderWidget::adjustHeaderTable(int type, QTableWidget *pTableWidget)
+{
+
+}
+
+void ELFSectionHeaderWidget::on_checkBoxReadonly_toggled(bool checked)
+{
+
+}
+
+void ELFSectionHeaderWidget::reloadData()
+{
+
+}
+
+void ELFSectionHeaderWidget::on_tableWidget_Section_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn)
+{
+
+}
