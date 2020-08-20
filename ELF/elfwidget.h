dfe38da3d7b63d6ec5d42b7537dd0f2d72413a87
horsicq <horsicq@gmail.com>
2020-08-20 09:12:55 +0200

    Fix

--- ELF/elfwidget.h
+++ ELF/elfwidget.h
@@ -56,7 +56,7 @@ private slots:
     virtual void reloadData();
     void addDatasets(XELF *pElf,QTreeWidgetItem *pParent, QList<XBinary::DATASET> *pList);
     void widgetValueChanged(quint64 nValue);
-    void on_treeWidgetNavi_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+    void on_treeWidgetNavi_currentItemChanged(QTreeWidgetItem *pCurrent, QTreeWidgetItem *pPrevious);
     void on_checkBoxReadonly_toggled(bool bChecked);
     bool createSectionTable(int nType, QTableWidget *pTableWidget, const FW_DEF::HEADER_RECORD *pRecords, int nRecordCount);
     void on_pushButtonReload_clicked();
