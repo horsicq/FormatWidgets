e5b5f305f5b32b784e05f28df568f4734816ae54
horsicq <horsicq@gmail.com>
2020-10-21 11:26:20 +0200

    Fix

--- LE/leprocessdata.h
+++ LE/leprocessdata.h
@@ -32,7 +32,7 @@ class LEProcessData : public ProcessData
 public:
     explicit LEProcessData(int nType,QStandardItemModel **ppModel,XLE *pLE,qint64 nOffset,qint64 nSize);
     virtual void _process();
-    virtual void ajustTableView(QWidget *pWidget, QTableView *pTableView);
+    virtual void ajustTableView(QWidget *pWidget,QTableView *pTableView);
 
 private:
     int nType;
