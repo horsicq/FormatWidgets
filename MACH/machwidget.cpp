b1b0ee4f5f8c628a9b63784ca26c943a18125219
horsicq <horsicq@gmail.com>
2021-11-08 22:43:38 +0100

    Fix: 2021-11-08

--- MACH/machwidget.cpp
+++ MACH/machwidget.cpp
@@ -2411,8 +2411,8 @@ void MACHWidget::reloadData()
 void MACHWidget::widgetValueChanged(quint64 nValue)
 {
     QWidget *pWidget=qobject_cast<QWidget *>(sender());
-    int nStype=pWidget->property("STYPE").toInt();
-    int nNdata=pWidget->property("NDATA").toInt();
+    qint32 nStype=pWidget->property("STYPE").toInt();
+    qint32 nNdata=pWidget->property("NDATA").toInt();
 
     switch(nStype)
     {
