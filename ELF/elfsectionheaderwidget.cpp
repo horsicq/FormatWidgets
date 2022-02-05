e0c076df28479a686a240380fef2e3b0bbaf61f0
horsicq <horsicq@gmail.com>
2022-02-06 00:18:48 +0100

    Fix: 2022-02-06

--- ELF/elfsectionheaderwidget.cpp
+++ ELF/elfsectionheaderwidget.cpp
@@ -726,7 +726,7 @@ void ELFSectionHeaderWidget::widgetValueChanged(quint64 nValue)
 {
     QWidget *pWidget=qobject_cast<QWidget *>(sender());
     qint32 nStype=pWidget->property("STYPE").toInt();
-    int nNdata=pWidget->property("NDATA").toInt();
+    qint32 nNdata=pWidget->property("NDATA").toInt();
 
     XELF elf(getDevice(),getOptions().bIsImage,getOptions().nImageBase);
 
