84ec39870baf71fcb7a359c1a9ffc1cb94be1da0
horsicq <horsicq@gmail.com>
2022-02-09 08:03:59 +0100

    Fix: 2022-02-09

--- Binary/binarywidget.cpp
+++ Binary/binarywidget.cpp
@@ -73,7 +73,7 @@ void BinaryWidget::reload()
     }
 }
 
-FormatWidget::SV BinaryWidget::_setValue(QVariant vValue, int nStype, int nNdata, int nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV BinaryWidget::_setValue(QVariant vValue,int nStype,int nNdata,int nVtype,int nPosition,qint64 nOffset)
 {
     Q_UNUSED(vValue)
     Q_UNUSED(nStype)
