d43353d11f13833d65d1d972f4942fd8b325ff62
horsicq <horsicq@gmail.com>
2022-03-22 07:10:17 +0100

    Fix: 2022-03-22

--- NE/nesectionheaderwidget.cpp
+++ NE/nesectionheaderwidget.cpp
@@ -50,7 +50,7 @@ void NESectionHeaderWidget::reload()
 
 }
 
-FormatWidget::SV NESectionHeaderWidget::_setValue(QVariant vValue, int nStype, int nNdata, int nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV NESectionHeaderWidget::_setValue(QVariant vValue,int nStype,int nNdata,int nVtype,int nPosition,qint64 nOffset)
 {
     Q_UNUSED(vValue)
     Q_UNUSED(nStype)
