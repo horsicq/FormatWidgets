7078949ac0a97331139d1e7441d2a553278edbce
horsicq <horsicq@gmail.com>
2022-03-27 03:52:33 +0200

    Fix: 2022-03-27

--- MACHOFAT/machofatwidget.cpp
+++ MACHOFAT/machofatwidget.cpp
@@ -100,7 +100,7 @@ void MACHOFATWidget::reload()
     }
 }
 
-FormatWidget::SV MACHOFATWidget::_setValue(QVariant vValue, int nStype, int nNdata, int nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV MACHOFATWidget::_setValue(QVariant vValue,int nStype,int nNdata,int nVtype,int nPosition,qint64 nOffset)
 {
     Q_UNUSED(vValue)
     Q_UNUSED(nStype)
