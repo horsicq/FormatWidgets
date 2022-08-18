a2b990af8b655d72bdf0e8ad518c4d3a119c69d2
horsicq <horsicq@gmail.com>
2022-08-18 07:38:33 +0200

    Fix: 2022-08-18

--- MACH/machwidget.cpp
+++ MACH/machwidget.cpp
@@ -2659,7 +2659,7 @@ void MACHWidget::on_tableWidget_unix_thread_x86_64_currentCellChanged(int nCurre
     setHeaderTableSelection(ui->widgetHex_unix_thread_x86_64,ui->tableWidget_unix_thread_x86_64);
 }
 
-void MACHWidget::on_tableWidget_unix_thread_arm_32_currentCellChanged(int nCurrentRow, int nCurrentColumn, int nPreviousRow, int nPreviousColumn)
+void MACHWidget::on_tableWidget_unix_thread_arm_32_currentCellChanged(int nCurrentRow,int nCurrentColumn,int nPreviousRow,int nPreviousColumn)
 {
     Q_UNUSED(nCurrentRow);
     Q_UNUSED(nCurrentColumn);
