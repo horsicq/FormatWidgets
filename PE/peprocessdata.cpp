23c7df5694ffdc24c341e94d7fdeb4c9fbe30f5c
horsicq <horsicq@gmail.com>
2022-10-05 22:44:30 +0200

    Fix: 2022-10-05

--- PE/peprocessdata.cpp
+++ PE/peprocessdata.cpp
@@ -314,7 +314,7 @@ void PEProcessData::_process()
 
         XBinary::_MEMORY_MAP memoryMap=g_pPE->getMemoryMap();
 
-        for(int i=0;(i<nNumberOfDelayImports)&&(isRun());i++)
+        for(qint32 i=0;(i<nNumberOfDelayImports)&&(isRun());i++)
         {
             QString sLibraryName=g_pPE->read_ansiString(g_pPE->relAddressToOffset(&memoryMap,listDelayImport.at(i).DllNameRVA));
 
