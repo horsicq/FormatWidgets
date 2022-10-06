6f442fdbdf942280c95bd41ea34c7ed6412a2876
horsicq <horsicq@gmail.com>
2022-10-06 18:59:30 +0200

    Fix: 2022-10-06

--- PE/peprocessdata.cpp
+++ PE/peprocessdata.cpp
@@ -422,7 +422,7 @@ void PEProcessData::_process()
 
         setHeader(*g_ppModel,&listLabels);
 
-        for(int i=0;(i<nNumberOfDebugs)&&(isRun());i++)
+        for(qint32 i=0;(i<nNumberOfDebugs)&&(isRun());i++)
         {
             QStandardItem *pItem=new QStandardItem;
             pItem->setData(i,Qt::DisplayRole);
