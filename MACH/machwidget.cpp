f20ffa7f39b8ea3c7c5ff29e570850663792482e
horsicq <horsicq@gmail.com>
2023-09-14 01:31:00 +0200

    Fix: 2023-09-14

--- MACH/machwidget.cpp
+++ MACH/machwidget.cpp
@@ -1174,7 +1174,7 @@ void MACHWidget::blockSignals(bool bState)
     _blockSignals((QObject **)g_comboBox, __CB_size, bState);
 }
 
-void MACHWidget::adjustHeaderTable(int nType, QTableWidget *pTableWidget)
+void MACHWidget::adjustHeaderTable(qint32 nType, QTableWidget *pTableWidget)
 {
     XBinary::MODE mode = XMACH::getMode(getDevice(), getOptions().bIsImage, getOptions().nImageBase);
 
