88150150cd5744f665af411a2d4db5cb48c7f610
horsicq <horsicq@gmail.com>
2023-10-15 13:36:54 +0200

    Update file(s): 2023-10-15

--- LE/lesectionheaderwidget.cpp
+++ LE/lesectionheaderwidget.cpp
@@ -45,7 +45,7 @@ void LESectionHeaderWidget::reload()
 {
 }
 
-FormatWidget::SV LESectionHeaderWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, qint32 nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV LESectionHeaderWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, qint32 nVtype, qint32 nPosition, qint64 nOffset)
 {
     Q_UNUSED(vValue)
     Q_UNUSED(nStype)
