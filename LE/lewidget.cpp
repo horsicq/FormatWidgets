a08f1c66db7cd4f491ffef8037b3e1d8e8a7eb4f
horsicq <horsicq@gmail.com>
2023-10-24 01:45:16 +0200

    Update file(s): 2023-10-24

--- LE/lewidget.cpp
+++ LE/lewidget.cpp
@@ -111,7 +111,7 @@ void LEWidget::reload()
     }
 }
 
-FormatWidget::SV LEWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, qint32 nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV LEWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, qint32 nVtype, qint32 nPosition, qint64 nOffset)
 {
     Q_UNUSED(nVtype)
     Q_UNUSED(nPosition)
