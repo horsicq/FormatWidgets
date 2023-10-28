5678a3601f819304183da52ec569eaadf6af3bc6
horsicq <horsicq@gmail.com>
2023-10-28 07:55:03 +0200

    Update file(s): 2023-10-28

--- DEX/dexwidget.cpp
+++ DEX/dexwidget.cpp
@@ -137,7 +137,7 @@ void DEXWidget::reload()
     }
 }
 
-FormatWidget::SV DEXWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, int nVtype, int nPosition, qint64 nOffset)
+FormatWidget::SV DEXWidget::_setValue(QVariant vValue, qint32 nStype, qint32 nNdata, qint32 nVtype, int nPosition, qint64 nOffset)
 {
     Q_UNUSED(nVtype)
     Q_UNUSED(nPosition)
