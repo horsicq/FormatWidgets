8ab792694555f159017fb82f824279052008291d
horsicq <horsicq@gmail.com>
2024-03-04 00:20:04 +0100

    Update file(s): 2024-03-04

--- Binary/binarywidget.cmake
+++ Binary/binarywidget.cmake
@@ -5,8 +5,11 @@ include(${CMAKE_CURRENT_LIST_DIR}/../formatwidget.cmake)
 set(BINARYWIDGET_SOURCES
     ${FORMATWIDGET_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/binary_defs.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/binary_defs.h
     ${CMAKE_CURRENT_LIST_DIR}/binarywidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/binarywidget.h
     ${CMAKE_CURRENT_LIST_DIR}/binarywidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogbinary.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogbinary.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogbinary.ui
     )
