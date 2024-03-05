54ac17691953f5929ef9d3f87218e5336b6105db
horsicq <horsicq@gmail.com>
2024-03-06 00:02:02 +0100

    Update file(s): 2024-03-06

--- LE/lewidget.cmake
+++ LE/lewidget.cmake
@@ -7,9 +7,14 @@ set(LEWIDGET_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/lesectionheaderwidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/lewidget.ui
     ${CMAKE_CURRENT_LIST_DIR}/dialogle.ui
-    ${CMAKE_CURRENT_LIST_DIR}/leprocessdata.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/lesectionheaderwidget.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/lewidget.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/le_defs.cpp
     ${CMAKE_CURRENT_LIST_DIR}/dialogle.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogle.h
+    ${CMAKE_CURRENT_LIST_DIR}/le_defs.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/le_defs.h
+    ${CMAKE_CURRENT_LIST_DIR}/leprocessdata.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/leprocessdata.h
+    ${CMAKE_CURRENT_LIST_DIR}/lesectionheaderwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/lesectionheaderwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/lewidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/lewidget.h
     )
