48c863652f25f39152f1c8ab7b26ade4e7aa468b
horsicq <horsicq@gmail.com>
2024-03-09 01:58:34 +0100

    Update file(s): 2024-03-09

--- SearchValues/searchvalueswidget.cmake
+++ SearchValues/searchvalueswidget.cmake
@@ -5,7 +5,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/../MultiSearch/multisearch.cmake)
 set(SEARCHVALUESWIDGET_SOURCES
     ${MULTISEARCH_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogsearchvalues.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogsearchvalues.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogsearchvalues.ui
     ${CMAKE_CURRENT_LIST_DIR}/searchvalueswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/searchvalueswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/searchvalueswidget.ui
     )
