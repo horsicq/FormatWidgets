7bb45150dd20a5c3e9a8d61ef84eefb152787144
horsicq <horsicq@gmail.com>
2024-12-11 01:17:43 +0100

    Update file(s): 2024-12-11

--- SearchStrings/searchstringswidget.cmake
+++ SearchStrings/searchstringswidget.cmake
@@ -4,8 +4,11 @@ if (NOT DEFINED MULTISEARCH_SOURCES)
     include(${CMAKE_CURRENT_LIST_DIR}/../MultiSearch/multisearch.cmake)
     set(SEARCHVALUESWIDGET_SOURCES ${SEARCHVALUESWIDGET_SOURCES} ${MULTISEARCH_SOURCES})
 endif()
+if (NOT DEFINED DIALOGEDITSTRING_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../../FormatDialogs/dialogeditstring.cmake)
+    set(SEARCHVALUESWIDGET_SOURCES ${SEARCHVALUESWIDGET_SOURCES} ${DIALOGEDITSTRING_SOURCES})
+endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/../../FormatDialogs/dialogeditstring.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../../Controls/xtableview.cmake)
 
 set(SEARCHSTRINGSWIDGET_SOURCES
