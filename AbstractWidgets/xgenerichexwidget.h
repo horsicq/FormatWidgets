f389626040e895dde2da0cbceec8e70529de2113
horsicq <horsicq@gmail.com>
2024-12-20 00:21:50 +0100

    Add new file(s): 2024-12-20

---
+++ AbstractWidgets/xgenerichexwidget.h
@@ -0,0 +1,43 @@
+/* Copyright (c) 2024 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef XGENERICHEXWIDGET_H
+#define XGENERICHEXWIDGET_H
+
+#include "xformatwidget.h"
+
+namespace Ui {
+class XGenericHexWidget;
+}
+
+class XGenericHexWidget : public XFormatWidget {
+    Q_OBJECT
+
+public:
+    explicit XGenericHexWidget(QWidget *pParent = nullptr);
+    ~XGenericHexWidget();
+    virtual void reloadData(bool bSaveSelection);
+    virtual void adjustView();
+
+private:
+    Ui::XGenericHexWidget *ui;
+};
+
+#endif  // XGENERICHEADERWIDGET_H
