8cad44f16ebc92138b07b6ee60297de788ae386c
horsicq <horsicq@gmail.com>
2025-05-21 21:44:45 +0200

    Add new file(s): 2025-05-21

---
+++ XAbstractWidgets/xabstractwidgets.cmake
@@ -0,0 +1,13 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XABSTRACTWIDGETS_SOURCES
+    ${XABSTRACTWIDGETS_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericabstractwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericabstractwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xmainwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xmainwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xmainwidget.ui
+)
