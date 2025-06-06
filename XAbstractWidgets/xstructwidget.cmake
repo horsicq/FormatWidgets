1cef4cf912150e1cbf936dd31c6dad050a97e83f
horsicq <horsicq@gmail.com>
2025-06-06 18:59:26 +0200

    Add new file(s): 2025-06-06

---
+++ XAbstractWidgets/xstructwidget.cmake
@@ -0,0 +1,19 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XSTRUCTWIDGET_SOURCES
+    ${XSTRUCTWIDGET_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericabstractwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericabstractwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xgenericheaderwidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xgenerictablewidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xgenerictablewidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xgenerictablewidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xstructwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xstructwidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xstructwidget.ui
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxstruct.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxstruct.h
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxstruct.ui
+)
---
+++ XAbstractWidgets/xstructwidget.pri
@@ -0,0 +1,22 @@
+INCLUDEPATH += $$PWD
+DEPENDPATH += $$PWD
+
+HEADERS += \
+    $$PWD/xgenericabstractwidget.h \
+    $$PWD/xgenericheaderwidget.h \
+    $$PWD/xgenerictablewidget.h \
+    $$PWD/dialogxstruct.h
+    $$PWD/xstructwidget.h
+
+SOURCES += \
+    $$PWD/xgenericabstractwidget.cpp \
+    $$PWD/xgenericheaderwidget.cpp \
+    $$PWD/xgenerictablewidget.cpp \
+    $$PWD/dialogxstruct.cpp
+    $$PWD/xstructwidget.cpp
+
+FORMS += \
+    $$PWD/xgenericheaderwidget.ui \
+    $$PWD/xgenerictablewidget.ui \
+    $$PWD/dialogxstruct.ui
+    $$PWD/xstructwidget.ui
